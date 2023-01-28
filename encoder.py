2e67a06d0eeface50057c5e31c4057589a7b4c58
bvagne <bvagne1@gmail.com>
2023-01-28 23:46:18 +0000

    adding encoder.py

---
+++ encoder.py
@@ -0,0 +1,19 @@
+import qiskit as qsk
+import numpy as np
+import skimage
+from skimage.transform import resize
+
+def make_circuit(image, n=16):
+    circuit = qsk.QuantumCircuit(n,n)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                circuit.rx(image[i][i],i)
+            else:
+                circuit.crx(image[i][j],i,j) #pixel (1,0)
+    return(circuit)
+
+def encode(image):
+    image = resize(np.random.sample((28,28)), (16, 16))
+    return(make_circuit(image))
+    
