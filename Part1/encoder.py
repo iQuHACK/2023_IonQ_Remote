f5b22b5cf1145834b604de91d7dc736bd7f28eba
Rodrigo Coelho <rodrigelho@gmail.com>
2023-01-28 17:13:25 +0000

    Update

--- Part1/encoder.py
+++ Part1/encoder.py
@@ -2,3 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from qiskit import QuantumCircuit, QuantumRegister
 
+def encoder(image):
+    n_qubits = 16
+    qc = QuantumCircuit(16)
+    for i, pixel_value in enumerate(np.flatten(image)):
+        if pixel_value:
+            qc.x(i)
+    return qc
