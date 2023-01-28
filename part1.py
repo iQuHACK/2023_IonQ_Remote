9e5e4ef3f11f6250ed443735bbe4d8233b0fbc64
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2023-01-28 19:09:52 +0100

    workingish with 3 qbits

--- part1.py
+++ part1.py
@@ -1,10 +1,15 @@
 import qiskit
+from qiskit import Aer
+import qiskit.circuit.library
+from qiskit import transpile, assemble
+from qiskit.visualization import plot_histogram
+
 import numpy as np
 import math
 
 SIZE = 28
 N = math.ceil(math.log2(SIZE))
-NB_QUBITS = 2*N + 1
+NB_QUBITS = 3 # 2*N + 1
 
 def load_images(path: str):
     return np.load(path)
@@ -25,9 +30,25 @@ def frqi_encode(image):
     for i in range(NB_QUBITS - 1):
         circuit.h(i)
     
-    # Apply the rotation gates wrt theta...
+    # Apply the rotation gates
+    theta = 0
+    c3ry = qiskit.circuit.library.RYGate(theta).control(2)
+    circuit.append(c3ry, [0, 1, 2])
+
+    circuit.x(1)
+    c3ry = qiskit.circuit.library.RYGate(theta).control(2)
+    circuit.append(c3ry, [0, 1, 2])
+
+    circuit.x(0)
+    circuit.x(1)
+    c3ry = qiskit.circuit.library.RYGate(theta).control(2)
+    circuit.append(c3ry, [0, 1, 2])
+
+    circuit.x(1)
+    c3ry = qiskit.circuit.library.RYGate(theta).control(2)
+    circuit.append(c3ry, [0, 1, 2])
     
-    
+    circuit.measure_all()
     # Print the circuit
     print(circuit)
     return circuit
@@ -35,3 +56,10 @@ def frqi_encode(image):
 if __name__ == "__main__":
     image = load_images('data/images.npy')[0]
     circuit = frqi_encode(image)
+    # Simulate the circuit
+    aer_sim = Aer.get_backend('aer_simulator')
+    t_qc = transpile(circuit, aer_sim)
+    qobj = assemble(t_qc, shots=4096)
+    result = aer_sim.run(qobj).result()
+    counts = result.get_counts(circuit)
+    print(counts)
