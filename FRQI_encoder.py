d0e863e0b07544ff3631314c097a81d76d98e700
Lawrence ARscott <arscott.lawrence@gmail.com>
2023-01-29 12:29:13 +0000

    Clean up FRQI_encode

--- FRQI_encoder.py
+++ FRQI_encoder.py
@@ -1,4 +1,5 @@
 ## FRQI image processing 28/01
+import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister, Aer, transpile, assemble
 from random import randint
 from math import pi, atan, sqrt
@@ -6,7 +7,7 @@ from math import pi, atan, sqrt
 n_scale = 3  # Use 2**n_scale colours
 n_pixels = 3  # Square image with height = length = 2**n_pixels
 
-test_image = [[randint(0, 2**n_scale - 1) / 2**n_scale * pi / 2 
+test_image = [[randint(0, 2**n_scale - 1) / 2**n_scale * pi / 2
                for i in range(2**n_pixels)] for j in range(2**n_pixels)]
 test_image[0][0] = 0
 print(test_image)
@@ -25,7 +26,12 @@ def x_gate_location(q_circuit, x_coord: str, y_coord: str, x_idx, y_idx):
         if y_coord[i] == '1':
             q_circuit.x(y_idx[n_pixels - i - 1])
 
-def encode(image, n_pix: int):
+def encode(image, n_pix: int, n_shots=4096, draw=False):
+    # Takes an image that is 2**n_pix by 2**n_pix pixels and encodes it
+    # Pixel with intensity theta (theta <= pi/2) will be encoded as
+    # cos theta |0yx> + sin theta |1yx>
+    # Final state is measured n_shots times. Counts are returned as a dictionary.
+
     # Initialize the quantum circuit for the image
     # Pixel states
     x_idx = QuantumRegister(n_pix, 'x_idx')
@@ -64,12 +70,15 @@ def encode(image, n_pix: int):
             qc_image.barrier()
 
     qc_image.measure_all()
-    qc_image.draw()
+
+    # Draw the circuit
+    if draw:
+        qc_image.draw()
 
     # Assemble and run circuit
     aer_sim = Aer.get_backend('aer_simulator')
     t_qc = transpile(qc_image, aer_sim)
-    qobj = assemble(t_qc, shots=4096)
+    qobj = assemble(t_qc, shots=n_shots)
     result = aer_sim.run(qobj).result()
     counts = result.get_counts(qc_image)
 
@@ -77,8 +86,11 @@ def encode(image, n_pix: int):
 
 
 ##
-def decode(dic, n_pix: int):
-    # Placeholder
+def decode(dic: dict, n_pix: int):
+    # Decodes an image that has been encoded with encode
+    # Image is 2**n_pix by 2**n_pix pixels
+
+    # Array to reconstitute image
     decoded = [[0 for _ in range(2**n_pix)] for _ in range(2**n_pix)]
 
     for i in range(2**n_pix):  # loop over x coordinates
@@ -88,18 +100,27 @@ def decode(dic, n_pix: int):
             key_0 = '0' + y_str + x_str
             key_1 = '1' + y_str + x_str
 
+            # If no measurements of |0ji> / |1ji>, the corresponding pixel has full/minimum intensity
+            # These cases are caught to avoid key errors
             if key_1 not in dic:
                 decoded[j][i] = 0
 
-            elif key_0 not in dic:
+            if key_0 not in dic:
                 decoded[j][i] = 1
 
             else:
                 tan2_theta = dic[key_1] / dic[key_0]
                 decoded[j][i] = atan(sqrt(tan2_theta))
 
-    return decoded
+    return np.array(decoded)
 
 
-##
+## Test random image
+print(test_image)
 print(decode(encode(test_image, n_pixels), n_pixels))
+
+## Test image from file
+images=np.load('data/images.npy')
+test_image = images[0]*255
+test_image = test_image[8:16, 8:16]
+ans = decode(encode(test_image, 3), 3)
