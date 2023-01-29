5294aef8c35935e16c6ed1417a7f2890811813e7
LionFrangoulis <lion.frangoulis@delos-consulting.com>
2023-01-29 14:48:32 +0000

    ADded files
    Added files

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import cirq
+import qiskit
+from qiskit import quantum_info
+from qiskit.execute_function import execute
+from qiskit import BasicAer
 import numpy as np
 import pickle
 import json
@@ -6,6 +9,8 @@ import os
 import sys
 from collections import Counter
 from sklearn.metrics import mean_squared_error
+from typing import Dict, List
+import matplotlib.pyplot as plt
 
 if len(sys.argv) > 1:
     data_path = sys.argv[1]
@@ -14,13 +19,12 @@ else:
 
 #define utility functions
 
-def simulate(circuit: cirq.Circuit) -> dict:
-    """This function simulates a Cirq circuit (without measurement) and outputs results in the format of histogram.
-    """
-    simulator = cirq.Simulator()
-    result = simulator.simulate(circuit)
-    
-    state_vector=result.final_state_vector
+def simulate(circuit: qiskit.QuantumCircuit) -> dict:
+    """Simulate the circuit, give the state vector as the result."""
+    backend = BasicAer.get_backend('statevector_simulator')
+    job = execute(circuit, backend)
+    result = job.result()
+    state_vector = result.get_statevector()
     
     histogram = dict()
     for i in range(len(state_vector)):
@@ -43,16 +47,16 @@ def histogram_to_category(histogram):
         
     return positive
 
-def count_gates(circuit: cirq.Circuit):
-    """Returns the number of 1-qubit gates, number of 2-qubit gates, number of 3-qubit gates...."""
-    counter=Counter([len(op.qubits) for op in circuit.all_operations()])
-    
+def count_gates(circuit: qiskit.QuantumCircuit) -> Dict[int, int]:
+    """Returns the number of gate operations with each number of qubits."""
+    counter = Counter([len(gate[1]) for gate in circuit.data])
     #feel free to comment out the following two lines. But make sure you don't have k-qubit gates in your circuit
     #for k>2
-    for i in range(2,20):
-        assert counter[i]==0
+    #for i in range(2,20):
+    #    assert counter[i]==0
         
     return counter
+
 
 def image_mse(image1,image2):
     # Using sklearns mean squared error:
@@ -73,6 +77,7 @@ def test():
     for image in images:
         #encode image into circuit
         circuit,image_re=run_part1(image)
+        image_re = np.asarray(image_re)
 
         #count the number of 2qubit gates used
         gatecount+=count_gates(circuit)[2]
@@ -111,20 +116,54 @@ def test():
     
     print(score_part1, ",", score_part2, ",", data_path, sep="")
 
+
 ############################
 #      YOUR CODE HERE      #
 ############################
+def half_image(image):
+    length=int(len(image)/2)
+    new_image=np.zeros((length,length))
+    for i in range(length):
+        for j in range(length):
+            new_image[i,j]=np.sum(image[2*i:2*i+2,2*j:2*j+2])
+    return(new_image)
+
+def rescale_image(image):
+    new_image=np.zeros((32,32))
+    for i in range(28):
+        for j in range(28):
+            new_image[i+2,j+2]=image[i,j]
+    return(half_image(new_image))
+
+def vectorize(image):
+    vector=image[0]
+    for line in image[1:]:
+        vector=np.concatenate((vector,line))
+    vector=vector/(np.linalg.norm(vector))
+    return(vector)
+
+# Functions 'encode' and 'decode' are dummy.
 def encode(image):
-    circuit=cirq.Circuit()
-    if image[0][0]==0:
-        circuit.append(cirq.rx(np.pi).on(cirq.LineQubit(0)))
-    return circuit
+    new_image=rescale_image(image)
+    print(new_image.shape)
+    statevector=vectorize(new_image)
+    circuit=qiskit.QuantumCircuit(8)
+    circuit.initialize(statevector)
+    circuit=circuit.decompose(reps=10)
+    return(circuit)
 
 def decode(histogram):
-    if 1 in histogram.keys():
-        image=np.array([[0,0],[0,0]])
-    else:
-        image=np.array([[1,1],[1,1]])
+    image_data=np.zeros((16*16))
+    for key in histogram.keys():
+        image_data[key]=histogram[key]
+    image=np.zeros((32,32))
+    for i in range(16):
+        for j in range(16):
+            image[2*i,2*j]=image_data[i*16+j]
+            image[2*i+1,2*j]=image_data[i*16+j]
+            image[2*i,2*j+1]=image_data[i*16+j]
+            image[2*i+1,2*j+1]=image_data[i*16+j]
+    image=image[2:30,2:30]
     return image
 
 def run_part1(image):
@@ -133,26 +172,27 @@ def run_part1(image):
 
     #simulate circuit
     histogram=simulate(circuit)
-
     #reconstruct the image
     image_re=decode(histogram)
-
     return circuit,image_re
 
 def run_part2(image):
     # load the quantum classifier circuit
-    with open('quantum_classifier.pickle', 'rb') as f:
-        classifier=pickle.load(f)
+    classifier=qiskit.QuantumCircuit.from_qasm_file('quantum_classifier.qasm')
     
     #encode image into circuit
     circuit=encode(image)
     
     #append with classifier circuit
-    
-    circuit.append(classifier)
+    nq1 = circuit.width()
+    nq2 = classifier.width()
+    nq = max(nq1, nq2)
+    qc = qiskit.QuantumCircuit(nq)
+    qc.append(circuit.to_instruction(), list(range(nq1)))
+    qc.append(classifier.to_instruction(), list(range(nq2)))
     
     #simulate circuit
-    histogram=simulate(circuit)
+    histogram=simulate(qc)
         
     #convert histogram to category
     label=histogram_to_category(histogram)
