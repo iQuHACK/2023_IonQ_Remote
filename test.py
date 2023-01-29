8843c1120c0487665a648b4755fe678a3361f6bd
Goddard Guryon <9369053+goddard-guryon@users.noreply.github.com>
2023-01-29 17:07:18 +0530

    Update and rename test_qiskit.py to test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import numpy as np
 import pickle
 import json
 import os
+import math
 import sys
 from collections import Counter
 from sklearn.metrics import mean_squared_error
@@ -121,18 +122,94 @@ def test():
 #      YOUR CODE HERE      #
 ############################
 def encode(image):
-    q = qiskit.QuantumRegister(3)
-    circuit = qiskit.QuantumCircuit(q)
-    if image[0][0]==0:
-        circuit.rx(np.pi,0)
-    return circuit
+    # initialize the circuit
+    row = qiskit.QuantumRegister(4, "row")
+    col = qiskit.QuantumRegister(4, "col")
+    val = qiskit.QuantumRegister(1, "val")
+    #cr = qiskit.QuantumRegister(3, "cr")
+    qc = qiskit.QuantumCircuit(row, col, val)
+
+    # assuming we have 28 x 28 image, first pad it to 32 x 32
+    img = np.pad(image, 2)
+    
+    # then downscale it to 16 x 16
+    x, y = img.shape
+    new_img = np.zeros((x//2, y//2))
+    
+    # simple average pooling
+    for i in range(x//2):
+        for j in range(y//2):
+            new_img[i, j] = (img[i*2, j*2] + img[i*2+1, j*2] + img[i*2, j*2+1] + img[i*2+1, j*2+1]) / 4
+    
+    # finally, bin the pixel intensities into 8 values
+    new_img *= 255**2
+    new_img[new_img<30] = 0
+    new_img[new_img>=230] = 0.875
+    new_img[new_img>=200] = 0.75
+    new_img[new_img>=165] = 0.625
+    new_img[new_img>=130] = 0.5
+    new_img[new_img>=100] = 0.375
+    new_img[new_img>=65] = 0.25
+    new_img[new_img>=30] = 0.125
+    new_img = (new_img*8).astype("uint8")
+    
+    # use FRQI for encoding this image, using 9 total qubits:
+    # 4 qubits for row, 4 for column, 1 for intensity
+    
+    # add Hadamard gates for input bits first
+    for i in range(8):
+        qc.h(i)
+    qc.barrier()
+    
+    # now set gates for intensity value of each pixel
+    for i in range(4):
+        for c1 in range(2):
+            for j in range(4):
+                for c2 in range(2):
+                    theta = new_img[i*(c1+1), j*(c2+1)]*math.pi/8
+                    r, c = i, j+4
+
+                    qc.cry(theta, r, 8)
+                    qc.cx(r, c)
+                    qc.cry(-theta, c, 8)
+                    qc.cx(r, c)
+                    qc.cry(theta, c, 8)
+                    qc.barrier()
+                    qc.x(c)
+                qc.x(r)
+    return qc
 
 def decode(histogram):
-    if 1 in histogram.keys():
-        image=[[0,0],[0,0]]
-    else:
-        image=[[1,1],[1,1]]
-    return image
+    r, c = 16, 16
+    # convert numbers to binary form
+    bin_hist = {f"{k:09b}": v for k, v in histogram.items()}
+    
+    # create an array for storing image
+    img = np.zeros((r, c))
+    
+    # get intensity at each pixel
+    for a in range(256):
+        i, j = a//r, a%c
+        try:
+            # I assume the ratio of |0> and |1> states for each pixel
+            # value is the same as the CRy angle theta
+            ones = bin_hist.get(f"1{a:08b}", 0)
+            zeros = bin_hist.get(f"0{a:08b}", 0)
+            img[i, j] = ones / (ones+zeros)
+        except KeyError:
+            img[i, j] = 0
+    #img = img.astype("uint8")
+    
+    # upscale 16x16 to 32x32
+    new_img = np.zeros((r*2, c*2))
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            val = img[i, j]
+            new_img[i*2, j*2] = new_img[i*2+1, j*2] = new_img[i*2, j*2+1] = new_img[i*2+1, j*2+1] = val
+    
+    # and remove padding
+    new_img = new_img[2:-2, 2:-2]
+    return new_img
 
 def run_part1(image):
     #encode image into a circuit
