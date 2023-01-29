17ad73315277e96c247190fa803cc56d0e4dd042
Kirin <chakhriya.chantharakhami@studentambassadors.com>
2023-01-29 21:25:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import cirq
+import qiskit
+from qiskit import quantum_info
+from qiskit.execute_function import execute
+from qiskit import BasicAer
 import numpy as np
 import pickle
 import json
@@ -6,6 +9,8 @@ import os
 import sys
 from collections import Counter
 from sklearn.metrics import mean_squared_error
+from typing import Dict, List
+import matplotlib.pyplot as plt
 
 if len(sys.argv) > 1:
     data_path = sys.argv[1]
@@ -14,13 +19,12 @@ else:
 
 #define utility functions
 
-def simulate(circuit: cirq.Circuit) -> dict:
-    """This function simulates a Cirq circuit (without measurement) and outputs results in the format of histogram.
-    """
-    simulator = cirq.Simulator()
-    result = simulator.simulate(circuit)
-    
-    state_vector=result.final_state_vector
+def simulate(circuit: qiskit.QuantumCircuit) -> dict:
+    """Simulate the circuit, give the state vector as the result."""
+    backend = BasicAer.get_backend('statevector_simulator')
+    job = execute(circuit, backend)
+    result = job.result()
+    state_vector = result.get_statevector()
     
     histogram = dict()
     for i in range(len(state_vector)):
@@ -43,16 +47,16 @@ def histogram_to_category(histogram):
         
     return positive
 
-def count_gates(circuit: cirq.Circuit):
-    """Returns the number of 1-qubit gates, number of 2-qubit gates, number of 3-qubit gates...."""
-    counter=Counter([len(op.qubits) for op in circuit.all_operations()])
-    
+def count_gates(circuit: qiskit.QuantumCircuit) -> Dict[int, int]:
+    """Returns the number of gate operations with each number of qubits."""
+    counter = Counter([len(gate[1]) for gate in circuit.data])
     #feel free to comment out the following two lines. But make sure you don't have k-qubit gates in your circuit
     #for k>2
     for i in range(2,20):
         assert counter[i]==0
         
     return counter
+
 
 def image_mse(image1,image2):
     # Using sklearns mean squared error:
@@ -73,6 +77,7 @@ def test():
     for image in images:
         #encode image into circuit
         circuit,image_re=run_part1(image)
+        image_re = np.asarray(image_re)
 
         #count the number of 2qubit gates used
         gatecount+=count_gates(circuit)[2]
@@ -111,21 +116,97 @@ def test():
     
     print(score_part1, ",", score_part2, ",", data_path, sep="")
 
+
 ############################
 #      YOUR CODE HERE      #
 ############################
 def encode(image):
-    circuit=cirq.Circuit()
-    if image[0][0]==0:
-        circuit.append(cirq.rx(np.pi).on(cirq.LineQubit(0)))
-    return circuit
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.decomposition import TruncatedSVD
+    from sklearn.manifold import TSNE
+
+    # The images are somehow 28 by 27 while the Kaggle says it is 28x28.
+    #     I don't know why they lost a row or column.
+    width = 28
+    length = 27
+    data_path = "./dataset/"
+
+    # Loading train data
+    train_data = np.load("/content/drive/MyDrive/2023_IonQ_Remote/data/images.npy")
+
+    # Showing an image
+    image = train_data[1, 1:].reshape((width, length))
+    plt.imshow(image)
+    plt.show()
+
+    #Extracting features and labels from the dataset and truncating the dataset to 10,000 datapoints
+    train_data_features = train_data[:10000, 1:]
+    train_data_labels = train_data[:10000, :1].reshape(-1,)
+    train_data_features = train_data_features.reshape(train_data_features.shape[0], -1)
+
+
+    # Using SVD to reduce dimensions to 10
+    tsvd = TruncatedSVD(n_components=2)
+    X_SVD = tsvd.fit_transform(train_data_features)
+
+    # Use t-SNE technique to reduce dimensions to 2
+    np.random.seed(0)
+    tsne = TSNE(n_components=2)
+    train_data_features_reduced = tsne.fit_transform(X_SVD)
+
+    zero_datapoints_array = [] #an array of the data points containing value 0
+    one_datapoints_array = []# an array of the data points containing value 1
+
+    # Iterate over the first 2000 samples of train_data_labels
+    for i in range(0,2000):
+        if train_data_labels[i] == 0:                   # extracting  0 is label for T-shirt
+            zero_datapoints_array.append(train_data_features_reduced[i])
+        elif train_data_labels[i] == 1:                   # extracting ones
+            one_datapoints_array.append(train_data_features_reduced[i])
+
+
+
+    zero_datapoints_array = np.array(zero_datapoints_array)
+    one_datapoints_array = np.array(one_datapoints_array)
+
+    def normalize(arr, max_val, n):
+        a = np.divide(arr, max_val)
+        return a + n
+
+    zero_datapoints_normalized = normalize(zero_datapoints_array, 100, 1)
+    one_datapoints_normalized = normalize(one_datapoints_array, 100, 1)
+
 
 def decode(histogram):
-    if 1 in histogram.keys():
-        image=np.array([[0,0],[0,0]])
-    else:
-        image=np.array([[1,1],[1,1]])
-    return image
+    #encode & decodehere
+    import qiskit
+    from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+
+    # Define the quantum and classical registers
+    q = QuantumRegister(16) # n is the number of qubits you want to use
+    c = ClassicalRegister(16)
+
+    # Create the quantum circuit
+    qc = QuantumCircuit(q, c)
+
+    # Iterate over each data point and apply rotations
+    for i in range(len(train_data_features_reduced)):
+        for j in range(2):
+            qc.ry(train_data_features_reduced[i][j], q[j])
+
+    # Measure the qubits and store the result in the classical register
+    qc.measure(q, c)
+
+    # Execute the circuit on a quantum backend
+    backend = qiskit.Aer.get_backend('qasm_simulator')
+    result = qiskit.execute(qc, backend, shots=1024).result()
+    counts = result.get_counts()
+
+    import matplotlib.pyplot as plt
+
+    plt.bar(counts.keys(), counts.values(), color='g')
+    plt.show()
 
 def run_part1(image):
     #encode image into a circuit
@@ -140,30 +221,87 @@ def run_part1(image):
     return circuit,image_re
 
 def run_part2(image):
-    # load the quantum classifier circuit
-    with open('quantum_classifier.pickle', 'rb') as f:
-        classifier=pickle.load(f)
-    
-    #encode image into circuit
-    circuit=encode(image)
-    
-    #append with classifier circuit
-    
-    circuit.append(classifier)
-    
-    #simulate circuit
-    histogram=simulate(circuit)
-        
-    #convert histogram to category
-    label=histogram_to_category(histogram)
-    
-    #thresholding the label, any way you want
-    if label>0.5:
-        label=1
-    else:
-        label=0
-        
-    return circuit,label
+    #part 2 cirq method 
+
+    import cirq
+    import numpy as np
+
+    # Slice the arrays to only include the first num_samples of each class
+
+    # Define the quantum circuit
+    qnn = cirq.Circuit()
+    qubits = cirq.LineQubit.range(8)
+
+    # Iterate over each data point and apply rotations
+    for i in range(len(one_datapoints_normalized)):
+        for j in range(10):
+            qnn.append(cirq.ry(one_datapoints_normalized[i][j]).on(qubits[j]))
+
+    # Measure the qubits and store the result in a classical register
+    qnn.append(cirq.measure(*qubits))
+
+    # Execute the circuit on a quantum simulator
+    simulator = cirq.Simulator()
+    zero_result = simulator.run(qnn, repetitions=1024)
+
+    # Define the quantum circuit
+    qnn = cirq.Circuit()
+    qubits = cirq.LineQubit.range(8)
+
+    # Iterate over each data point and apply rotations
+    for i in range(len(one_datapoints_normalized)):
+        for j in range(2):
+            qnn.append(cirq.ry(one_datapoints_normalized[i][j]).on(qubits[j]))
+
+    # Measure the qubits and store the result in a classical register
+    qnn.append(cirq.measure(*qubits))
+
+    # Execute the circuit on a quantum simulator
+    simulator = cirq.Simulator()
+    one_result = simulator.run(qnn, repetitions=1024)
+
+    qubits = cirq.LineQubit.range(16)
+    circuit = cirq.Circuit()
+    for i in range(len(zero_datapoints_normalized)):
+        for j in range(2):
+            circuit.append(cirq.ry(zero_datapoints_normalized[i][j]).on(qubits[j]))
+
+    # Apply quantum gates
+    circuit.append(cirq.CNOT(qubits[0], qubits[1]))
+    circuit.append(cirq.CZ(qubits[0], qubits[2]))
+    circuit.append(cirq.CNOT(qubits[1], qubits[3]))
+
+    # Measure the qubits
+    for i in range(16):
+        circuit.append(cirq.measure(qubits[i], key=str(i)))
+
+
+    #shape
+    print (zero_datapoints_normalized.shape)
+    print (train_data_labels.shape)
+
+    # Now you can use the zero_data and one_data arrays in your Q
+    from sklearn.svm import SVC
+    clf = SVC()
+    train_data_labels_reshaped = train_data_labels.reshape(-1, 1)
+    print(len(train_data_labels_reshaped))
+
+    num_samples = min(len(zero_datapoints_normalized), len(train_data_labels))
+    zero_datapoints_normalized = zero_datapoints_normalized[:num_samples]
+    train_data_labels = train_data_labels[:num_samples]
+
+    threshold = 0.5
+    import numpy as np
+    # Convert continuous labels to 0s and 1s
+    train_data_labels = np.where(train_data_labels > threshold, 1, 0)
+    zero_datapoints_normalized = np.where(zero_datapoints_normalized > threshold, 1, 0)
+
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(zero_datapoints_normalized, train_data_labels, test_size=0.2, random_state=42)
+
+    from sklearn.svm import OneClassSVM
+    clf = OneClassSVM()
+    clf.fit(X_train)
 
 ############################
 #      END YOUR CODE       #
