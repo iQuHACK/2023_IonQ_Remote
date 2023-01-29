7369a89095566b28cb058e7ff96a474de1108b81
Stefan Hill <hill.heide@web.de>
2023-01-29 09:15:48 +0000

    classifier circuit simulator classes

--- classifier.py
+++ classifier.py
@@ -6,18 +6,7 @@ import jax.numpy as jnp
 import jax
 import numpy as np
 
-BATCH_SIZE = 32
-EPOCHS = 1_000
 
-# TODO: replace with weight matrix
-# weight matrix has shape [16 * layers]
-initial_params = {
-    'hidden': jax.random.normal(shape=[784, 32], key=jax.random.PRNGKey(0)),
-    'output': jax.random.normal(shape=[32, 2], key=jax.random.PRNGKey(1)),
-}
-
-# TODO interface params = {'w': np.ndarray}
-# params[w][
 
 def variational_circuit(x: jnp.ndarray, params: jnp.ndarray, n_layer: int) -> jnp.ndarray:
   # TODO: this has to be replaced with our circuit where x is fed into the encoder and params['w'] is fed into 
@@ -31,81 +20,24 @@ def variational_circuit(x: jnp.ndarray, params: jnp.ndarray, n_layer: int) -> jn
     
   return x
 
-def circuit_wrapper(x: jnp.ndarray, params: jnp.ndarray, n_layer: int) -> jnp.ndarray:
-    circuit = variational_circuit(x, params, n_layers)
-    
-    # TODO init qiskit
-    # TODO simulate circuit
-    
-    # TODO histogram to label
-    
-    # TODO return label
-    
-    pass
-
-def loss(params: optax.Params, batch: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
-  y_hat = variational_circuit(batch, params)
-    
-    # TODO converter histogram_to_label
-  # optax also provides a number of common loss functions.
-  loss_value = optax.sigmoid_binary_cross_entropy(y_hat, labels).sum(axis=-1)
-
-  return loss_value.mean()
-
-def fit(params: optax.Params, optimizer: optax.GradientTransformation) -> optax.Params:
-  opt_state = optimizer.init(params)
-
-  @jax.jit
-  def step(params, opt_state, batch, labels):
-    loss_value, grads = jax.value_and_grad(loss)(params, batch, labels)
-    updates, opt_state = optimizer.update(grads, opt_state, params)
-    params = optax.apply_updates(params, updates)
-    return params, opt_state, loss_value
-
-  for i in range(EPOCHS):
-    batch_index = np.random.randint(0, 1340, (BATCH_SIZE,))
-    x_train_batch = X_train_res[batch_index]
-    y_train_batch = y_train[batch_index]
-    
-    #print(x_train_batch.shape)
-    #print(y_train_batch.shape)
-    
-    
-    params, opt_state, loss_value = step(params, opt_state, x_train_batch, y_train_batch)
-    losses.append(loss_value)
-    if i % 100 == 0:
-      print(f'step {i}, loss: {loss_value}')
-
-  return params
-
-# Finally, we can fit our parametrized function using the Adam optimizer
-# provided by optax.
-losses = []
 
 
-def circuit_wrapper(circuit_fn, x: jnp.ndarray, params: jnp.ndarray) -> jnp.ndarray:
-    circuit = circuit_fn(x, params)
-    
-    
-    
-    # TODO init qiskit
-    # TODO simulate circuit
-    
-    # TODO histogram to label
-    
-    # TODO return label
-    
-    pass
-
-
-
-
-
-def run_training(X, y, circuit:'Circuit', loss_fn, optimizer):
+def run_training(X, y, circuit:'Circuit', simulator:'Simulator', optimizer=None):
     
     n_layers = 2
     batch_size = 32
     epochs = 1_000
+    
+    def circuit_wrapper(x: jnp.ndarray, params: jnp.ndarray) -> jnp.ndarray:
+        circuit_fn = circuit.encode(x, params)
+        
+        simulator = simulate.run(circuit_fn)
+
+        # TODO histogram to label
+
+        # TODO return label
+        pass
+
     
     def loss(params: optax.Params, batch: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
         y_hat = circuit_wrapper(batch, params, circuit_fn)
@@ -121,16 +53,16 @@ def run_training(X, y, circuit:'Circuit', loss_fn, optimizer):
 
         @jax.jit
         def step(params, opt_state, batch, labels):
-            loss_value, grads = jax.value_and_grad(loss)(circuit, params, batch, labels)
+            loss_value, grads = jax.value_and_grad(loss)(params, batch, labels)
             updates, opt_state = optimizer.update(grads, opt_state, params)
             params = optax.apply_updates(params, updates)
             return params, opt_state, loss_value
 
         for i in range(epochs):
 
-            batch_index = np.random.randint(0, len, (batch_size,))
-            x_train_batch = X_train_res[batch_index]
-            y_train_batch = y_train[batch_index]
+            batch_index = np.random.randint(0, len(y), (batch_size,))
+            x_train_batch = X[batch_index]
+            y_train_batch = y[batch_index]
 
 
             params, opt_state, loss_value = step(params, opt_state, x_train_batch, y_train_batch)
@@ -145,7 +77,8 @@ def run_training(X, y, circuit:'Circuit', loss_fn, optimizer):
         'w': jax.random.normal(shape=[16, n_layers], key=jax.random.PRNGKey(0)),
     }
     
-    optimizer = optax.adam(learning_rate=1e-2)
+    if optimizer is None:
+        optimizer = optax.adam(learning_rate=1e-2)
     
     params, losses = fit(initial_params, optimizer, circuit)
     
