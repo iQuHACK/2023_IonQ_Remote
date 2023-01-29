968b51182b1b80e9fef5878741c86424aa6f9bc0
Louis-Justin TALLOT <lj.tallot@gmail.com>
2023-01-29 15:54:59 +0100

    Final submission

--- test.py
+++ test.py
@@ -120,19 +120,133 @@ def test():
 ############################
 #      YOUR CODE HERE      #
 ############################
+import math
+# Image properties
+SIZE = 4 # Image width
+NB_PX_IMG = SIZE ** 2
+
+# quantum parameters
+N = math.ceil(math.log2(SIZE))
+NB_QUBITS = 2 * N + 1
+NB_PX = 2 ** (2 * N)
+
+
+
+def pixel_value_to_theta(pixel: float) -> float:
+    return pixel / 255 * (np.pi / 2)
+
+def theta_to_pixel_value(theta: float) -> int:
+    return int(theta / (np.pi / 2) * 255)
+
+def get_proba(counts: dict) -> dict:
+    sums = sum(map(lambda x: x[1], counts.items()))
+    return {key: value / sums for key, value in counts.items()}
+
+
+def recursive_ry(circuit, theta, mask):
+    if mask.sum() == 2:
+        idxs = np.where(mask == 1)[0]
+        circuit.cry(theta/2, idxs[0], NB_QUBITS - 1)
+        circuit.cx(idxs[0], idxs[1])
+        circuit.cry(-theta/2, idxs[1], NB_QUBITS - 1)
+        circuit.cx(idxs[0], idxs[1])
+        circuit.cry(theta/2, idxs[1], NB_QUBITS - 1)
+    else:
+        idx1, idx2 = np.where(mask == 1)[0][:2]
+        
+        maskn = mask.copy()
+        maskn[idx2] = 0
+        recursive_ry(circuit, theta/2, maskn)
+        # c3ry = RYGate(theta).control(NB_QUBITS - 1)
+        # circuit.append(c3ry, ry_qbits)
+        circuit.cx(idx1, idx2)
+
+        maskn = mask.copy()
+        maskn[idx1] = 0
+        recursive_ry(circuit, -theta/2, maskn)
+        circuit.cx(idx1, idx2)
+
+        maskn = mask.copy()
+        maskn[idx1] = 0
+        recursive_ry(circuit, theta/2, maskn)
+
+
+
+
 def encode(image):
-    q = qiskit.QuantumRegister(3)
-    circuit = qiskit.QuantumCircuit(q)
-    if image[0][0]==0:
-        circuit.rx(np.pi,0)
+    circuit = qiskit.QuantumCircuit(NB_QUBITS)
+    image = image[::7, ::7]
+    # Get the theta values for each pixel
+    image = image.flatten()
+    thetas = [pixel_value_to_theta(pixel) for pixel in image]
+    thetas += [0] * (NB_PX - NB_PX_IMG)
+
+    # Apply Hadamard gates for all qubits except the last one
+    for i in range(NB_QUBITS - 1):
+        circuit.h(i)
+
+    switches = [bin(0)[2:].zfill(NB_QUBITS - 1)] + [
+        bin(i ^ (i - 1))[2:].zfill(NB_QUBITS - 1) for i in range(1, NB_PX)
+    ]
+
+
+    # Apply the rotation gates
+    prev_switch = switches[0]
+    for i in range(NB_PX):
+        theta = thetas[i] # pixel_value_to_theta(intensity_count_expression[i][0])
+        
+        # do not do zero rotation
+        if theta != 0:
+            switch = np.binary_repr(i, NB_QUBITS - 1)
+
+            # Apply x gate to the i-th qubit if the i-th bit of the switch is 1
+            for j in range(NB_QUBITS - 1):
+                if switch[j] != prev_switch[j]:
+                    circuit.x(j)
+                # if switch[j] == "1":
+                #     circuit.x(j - 1)
+            prev_switch = switch
+
+            recursive_ry(circuit, 2*theta, np.array([1]*(NB_QUBITS - 1)))
+
+            #circuit.barrier()
+
+    # check all qbits are at 1 for the measurements
+    for j in range(NB_QUBITS - 1):
+        if prev_switch[j] != "1":
+            circuit.x(j)
+
+    circuit.measure_all()
     return circuit
 
-def decode(histogram):
-    if 1 in histogram.keys():
-        image=[[0,0],[0,0]]
-    else:
-        image=[[1,1],[1,1]]
-    return image
+def decode(counts: dict) -> np.ndarray:
+    histogram = get_proba(counts)
+    img = np.zeros(NB_PX)  # we have a square image
+
+    for i in range(NB_PX):
+        print(i)
+        bin_str: str = np.binary_repr(i, width=NB_QUBITS - 1)
+        print(bin_str)
+        cos_str = "0" + bin_str[::-1]
+        sin_str = "1" + bin_str[::-1]
+
+        if cos_str in histogram:
+            prob_cos = histogram[cos_str]
+            theta = math.acos(np.clip(2**N * math.sqrt(prob_cos), 0, 1))
+        else:
+            prob_cos = 0
+
+        # not needed?
+        if sin_str in histogram:
+            prob_sin = histogram[sin_str]
+            theta = math.asin(np.clip(2**N * math.sqrt(prob_sin), 0, 1))
+        else:
+            prob_sin = 0
+
+        img[i] = theta_to_pixel_value(theta)
+
+    img = img[:NB_PX_IMG]
+    return img.reshape(SIZE, SIZE)
 
 def run_part1(image):
     #encode image into a circuit
